8d2f62b1ddb299d8623e0cde9410a0dc994c2ab7
wendelti <wendel.tavares.ti@gmail.com>
2018-11-17 07:00:00 -0200

    INCEPTION

---
+++ DNNModule3/View.ascx
@@ -0,0 +1,3 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="View.ascx.cs" Inherits="DnnBaseDNNModule3.View" %>
+
+<asp:Label runat="server" ID="lblQuerystring"></asp:Label>
---
+++ DNNModule3/View.ascx.cs
@@ -0,0 +1,81 @@
+﻿/*
+' Copyright (c) 2018  DnnBase
+'  All rights reserved.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+' TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+' THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+' CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+' DEALINGS IN THE SOFTWARE.
+' 
+*/
+
+using System;
+using DotNetNuke.Security;
+using DotNetNuke.Services.Exceptions;
+using DotNetNuke.Entities.Modules;
+using DotNetNuke.Entities.Modules.Actions;
+using DotNetNuke.Services.Localization;
+using DotNetNuke.Data;
+
+namespace DnnBaseDNNModule3
+{
+    /// -----------------------------------------------------------------------------
+    /// <summary>
+    /// The View class displays the content
+    /// 
+    /// Typically your view control would be used to display content or functionality in your module.
+    /// 
+    /// View may be the only control you have in your project depending on the complexity of your module
+    /// 
+    /// Because the control inherits from DNNModule3ModuleBase you have access to any custom properties
+    /// defined there, as well as properties from DNN such as PortalId, ModuleId, TabId, UserId and many more.
+    /// 
+    /// </summary>
+    /// -----------------------------------------------------------------------------
+    public partial class View : DNNModule3ModuleBase, IActionable
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                lblQuerystring.Text = Request.QueryString.ToString();
+
+                var tabId = Convert.ToInt32(Request.QueryString["TabId"]);
+                lblQuerystring.Text += $"<br>{tabId}";
+
+                var reader = (DataProvider.Instance().ExecuteSQL($"SELECT TabName, TabPath, Description FROM Tabs WHERE ParentId = {tabId}"));
+
+                while (reader.Read())
+                {
+                    var tabName = reader.GetString(0);
+                    var Url = reader.GetString(1);
+                    var Description = reader.GetString(2);
+                    lblQuerystring.Text += $"<h2><a href='{Url.Replace("//","/")}'>{tabName}</a></h2><p>{Description}</p>";
+                }
+
+
+
+            }
+            catch (Exception exc) //Module failed to load
+            {
+                Exceptions.ProcessModuleLoadException(this, exc);
+            }
+        }
+
+        public ModuleActionCollection ModuleActions
+        {
+            get
+            {
+                var actions = new ModuleActionCollection
+                    {
+                        {
+                            GetNextActionID(), Localization.GetString("EditModule", LocalResourceFile), "", "", "",
+                            EditUrl(), false, SecurityAccessLevel.Edit, true, false
+                        }
+                    };
+                return actions;
+            }
+        }
+    }
+}
